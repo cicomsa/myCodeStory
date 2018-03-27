37a78ec7dfbff45ef96de8bf700d3fa457adf850
cicomsa <ioanacomsa85@gmail.com>
2018-03-27 02:54:38 +0200

    '' removed

--- week2/design.js
+++ week2/design.js
@@ -1,7 +1,7 @@
-var images = ['"./annie-spratt-44241-unsplash.jpg"', "./annie-spratt-109237-unsplash.jpg", '"./annie-spratt-144480-unsplash.jpg"',
-'"./annie-spratt-439330-unsplash.jpg"','"./eddie-garcia-524055-unsplash.jpg"', '"./florian-giorgio-121817-unsplash.jpg"', '"./joshua-ness-109299-unsplash"',
-'"./kari-shea-110409-unsplash.jpg"', '"./karolina-szczur-504621-unsplash.jpg"', '"./malte-wingen-355701-unsplash.jpg"','"./ostap-senyuk-443230-unsplash"',
-'"./patrick-schneider-146210-unsplash.jpg"', '"./redd-angelo-249352-unsplash.jpg"', '"./tom-holmes-556804-unsplash.jpg"']
+var images = ["./annie-spratt-44241-unsplash.jpg", "./annie-spratt-109237-unsplash.jpg", "./annie-spratt-144480-unsplash.jpg",
+"./annie-spratt-439330-unsplash.jpg","./eddie-garcia-524055-unsplash.jpg", "./florian-giorgio-121817-unsplash.jpg", "./joshua-ness-109299-unsplash",
+"./kari-shea-110409-unsplash.jpg", "./karolina-szczur-504621-unsplash.jpg", "./malte-wingen-355701-unsplash.jpg","./ostap-senyuk-443230-unsplash",
+"./patrick-schneider-146210-unsplash.jpg", "./redd-angelo-249352-unsplash.jpg", "./tom-holmes-556804-unsplash.jpg"]
 
 
 
@@ -10,7 +10,7 @@ window.onload = function () {
   image = document.createElement('img');
   //image.id = "image2"
 
-  image.src = images[1];
+  image.src = images[4];
   source=document.getElementsByClassName("box2")
   $("#image1").replaceWith(image)
   //img = document.querySelector("#image1")
