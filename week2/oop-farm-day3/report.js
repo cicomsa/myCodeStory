6b85b894415db77764a69c4df3d9a78f7f3ba206
cicomsa <ioanacomsa85@gmail.com>
2018-05-28 19:38:18 +0200

    removed unnecesary projects

--- week2/oop-farm-day3/report.js
+++
@@ -1,6 +0,0 @@
-const {Farm, animalsAndCrops} = require('./index')
-const {Wheat, Sugarcane} = require('./crops')
-const {Cow, Pig, Horse} = require('./animals')
-
-const myFarm = new Farm("FARM")
-console.log(`Farm: ${myFarm.name}`)
